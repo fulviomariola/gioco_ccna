cd2c0a28bbfbc1b4bcea24c6d6eb07e566e3a670
Fulvio Mariola <zingaroweb@Fulvios-MacBook-Pro.local>
2019-03-02 11:55:19 +0100

    primo commit

---
+++ src/bin_to_dec/DecToBin.java
@@ -0,0 +1,28 @@
+package bin_to_dec;
+
+import java.util.Scanner;
+
+public class DecToBin {
+
+	public static void main(String[] args) {
+        int n, count = 0, a;
+        String x = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Inserisci un numero decimale: ");
+        n = s.nextInt();
+        while(n > 0)
+        {
+            a = n % 2;
+            if(a == 1)
+            {
+                count++;
+            }
+            x = x + "" + a;
+            n = n / 2;
+        }
+        System.out.println("Numero binario: "+x);
+        System.out.println("Numero di 1: "+count);
+
+	}
+
+}
